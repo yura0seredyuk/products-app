195fa5f11c9292a496d8f324a9ff226b8c3d14f3
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-04-13 20:30:36 +0300

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import firebase from 'firebase/app';
+
 function App() {
+  const [products, setProducts] = useState([]);
+  const ref = firebase.firestore().collection('Products');
+
+  function getProducts() {
+    ref.onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((item) => {
+        items.push(item.data());
+      });
+      setProducts(items);
+    });
+  }
+
+  useEffect(() => {
+    getProducts();
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-
+          {console.log(products)}
         </p>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+require('firebase/database');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSZDPFopZP_72U7aGNOjbxQQB4f618zbQ",
+  authDomain: "products-app-5d772.firebaseapp.com",
+  projectId: "products-app-5d772",
+  storageBucket: "products-app-5d772.appspot.com",
+  messagingSenderId: "635234318516",
+  appId: "1:635234318516:web:0d9169d9726648894b7fe1"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
